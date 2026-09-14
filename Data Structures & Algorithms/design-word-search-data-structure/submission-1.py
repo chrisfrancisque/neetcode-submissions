b7e51179d2cd04733a9aa72b07acede69421219f
Christopher Francisque <chrisfrancisque1@gmail.com>
2026-09-14 16:47:41 -0400

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.py
@@ -0,0 +1,45 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isEndOf = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+        
+
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.isEndOf = True
+
+        
+
+    def search(self, word: str) -> bool:
+
+        def dfs(j, root):
+            node = root
+            for i in range(j, len(word)):
+                char = word[i]
+                if char == '.':
+                    for child in node.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                
+                else:
+                    if char not in node.children:
+                        return False
+                    node = node.children[char]
+            return node.isEndOf
+        
+        return dfs(0, self.root)
+            
+
+
+       
+        
